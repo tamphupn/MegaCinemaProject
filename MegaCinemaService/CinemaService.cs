2c179b28c20ce3eb1a93a2260af365f8bd2eda96
eitguide <eitguide.np@gmail.com>
2016-11-22 18:43:35 -0800

    CinemaController

---
+++ MegaCinemaService/CinemaService.cs
@@ -0,0 +1,47 @@
+﻿using MegaCinemaData.Infrastructures;
+using MegaCinemaData.Repositories;
+using MegaCinemaModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaCinemaService
+{
+
+    public interface ICinemaService
+    {
+        //defined method
+        IEnumerable<Cinema> GetAll();
+        Cinema Add(Cinema cinema);
+        IEnumerable<Cinema> GetFoodListPaging(int page, int pageSize, out int totalRow);
+        void SaveChanges();
+    }
+
+    public class CinemaService : ICinemaService
+    {
+        private IFoodListRepository _foodListRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public Cinema Add(Cinema cinema)
+        {
+            return null;
+        }
+
+        public IEnumerable<Cinema> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Cinema> GetFoodListPaging(int page, int pageSize, out int totalRow)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MegaCinemaWeb/Areas/AdminDashboard/Controllers/CinemaController.cs
@@ -0,0 +1,47 @@
+﻿using MegaCinemaService;
+using MegaCinemaWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MegaCinemaWeb.Areas.AdminDashboard.Controllers
+{
+    public class CinemaController : Controller
+    {
+        ICinemaService _foodListService;
+        IStatusService _statusService;
+        public CinemaController(ICinemaService foodListService, IStatusService statusService)
+        {
+            _foodListService = foodListService;
+            _statusService = statusService;
+        }
+        // GET: AdminDashboard/Cinema
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.CinemaStatus = new SelectList(_statusService.GetAll(), "StatusID", "StatusName");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CinemaViewModel cinema)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            //Action/Controller
+            return RedirectToAction("Index", "FoodList");
+           
+        }
+    }
+}
