9d411d9f78191111792288470e9e7034af1afa61
eitguide <eitguide.np@gmail.com>
2016-11-27 17:56:07 +0700

    Create StaffRepository and StaffService

---
+++ MegaCinemaData/Repositories/StaffRepository.cs
@@ -0,0 +1,25 @@
+﻿using MegaCinemaData.Infrastructures;
+using MegaCinemaModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaCinemaData.Repositories
+{
+   
+
+    public interface IStaffRepository : IRepository<Staff>
+    {
+
+    }
+
+    public class StaffRepository : RepositoryBase<Staff>, IStaffRepository
+    {
+        public StaffRepository(IDbFactory dbfactory) : base(dbfactory)
+        {
+
+        }
+    }
+}
---
+++ MegaCinemaService/StaffService.cs
@@ -0,0 +1,33 @@
+﻿using MegaCinemaData.Infrastructures;
+using MegaCinemaData.Repositories;
+using MegaCinemaModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaCinemaService
+{
+    public interface IStaffService
+    {
+        IEnumerable<Staff> GetAll();
+    }
+
+    public class StaffService : IStaffService
+    {
+        private IStaffRepository _staffRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public StaffService(IStaffRepository staffRepository, IUnitOfWork unitOfWork)
+        {
+            _staffRepository = staffRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<Staff> GetAll()
+        {
+           return _staffRepository.GetAll();
+        }
+    }
+}
